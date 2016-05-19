8181939cf3b11cd57069a639a198b2cd0d5717e9
adam-s <adamsohn_1@hotmail.com>
2016-05-19 03:24:01 -0400

    starting mark 3

--- client/scripts/states/menu/menu.routes.js
+++ client/scripts/states/menu/menu.routes.js
@@ -5,94 +5,13 @@
     router.$inject = ['$stateProvider'];
 
     function router($stateProvider) {
-
-        $stateProvider
-            .state('menu', {
-                parent: 'layout-sidenav',
-                views: {
-                    'content@layout-sidenav': {
-                        templateUrl: 'scripts/states/menu/menu.tpl.html'
-                    },
-                    'context@menu': {
-                        templateProvider: ['contextTemplate', function(contextTemplate) {
-                            return contextTemplate;
-                        }],
-                        controllerAs: 'vm',
-                        controllerProvider: ['contextController', function(contextController) {
-                            return contextController;
-                        }]
-                    },
-                    'preview@menu': {
-                        templateProvider: ['previewTemplate', function(previewTemplate) {
-                            return previewTemplate;
-                        }],
-                        controllerAs: 'vm',
-                        controllerProvider: ['previewController', function(previewController) {
-                            return previewController;
-                        }]
-                    }
-                },
-                resolve: {
-                    config: config,
-                    context: context,
-                    contextController: contextController,
-                    contextTemplate: contextTemplate,
-                    previewController: previewController,
-                    previewTemplate: previewTemplate
-                },
-                params: {
-                    item: null,
-                    options: null
+        $stateProvider('menu', {
+            parent: 'layout-sidenav',
+            views: {
+                'context@layout-sidenav': {
+                    templateUrl: 'scripts/states/menu/menu.tpl.html'
                 }
-            });
-
-        $stateProvider
-            .state('menuContext', {
-                url: '/menu',
-                parent: 'menu'
-            });
-
-        $stateProvider
-            .state('menuAdd', {
-                url: '/menu/add',
-                parent: 'menu',
-                resolve: {
-                    context: function() {
-                        return 'fuck';
-                    }
-                }
-            });
-
-        config.$inject = ['$stateParams', 'TypesItemFactory'];
-        function config($stateParams, TypesItemFactory){
-            console.log('changing');
-            return TypesItemFactory($stateParams);
-        }
-
-        context.$inject = ['config'];
-        function context(config) {
-            return config.wrapper;
-        }
-
-        contextController.$inject = ['config'];
-        function contextController(config) {
-            return config.getContextController();
-        }
-
-        contextTemplate.$inject = ['config'];
-        function contextTemplate(config) {
-            return config.getContextTemplate();
-        }
-
-        previewController.$inject = ['config'];
-        function previewController(config) {
-            return config.getPreviewController();
-        }
-
-        previewTemplate.$inject = ['config'];
-        function previewTemplate(config) {
-            return config.getPreviewTemplate();
-        }
+            }
+        })
     }
-
 })();
--- client/scripts/states/menu/menu.srv.js
+++ client/scripts/states/menu/menu.srv.js
@@ -2,98 +2,11 @@
     angular.module('quickie')
         .factory('menu', menu);
 
-    menu.$inject = ['$timeout', '$q', 'menuApi', 'MenuTree'];
+    menu.$inject = [];
 
-    function menu($timeout, $q, menuApi, MenuTree) {
-        // CRUD on each node
-        var root = {};
-        var activeContext = {};
-
-        var menuTypes = [
-            {
-                name: 'Container',
-                value: 'container'
-            },
-            {
-                name: 'Section',
-                value: 'section'
-            },
-            {
-                name: 'Items group',
-                value: 'group'
-            },
-            {
-                name: 'Menu item',
-                value: 'item'
-            }
-        ];
-
+    function menu() {
         return {
-            init: init,
-            getActiveContext: getActiveContext,
-            setActiveContext: setActiveContext,
-            orderContextChildren: orderContextChildren,
-            addItem: addItem,
-            dropItem: dropItem,
-            getBreadcrumb: getBreadcrumb,
-            getTypes: getTypes
 
-        };
-
-        function init() {
-            menuApi.fetchMenu()
-                .then(function(response) {
-                    activeContext = new MenuTree(response);
-                    setRootClosure(activeContext);
-                });
         }
-
-        // The root is set in closure
-        function setRootClosure(context) {
-            return (function() {
-                root = context;
-            })();
-        }
-
-        function getActiveContext() {
-            return activeContext;
-        }
-
-        function setActiveContext(context) {
-            activeContext = context;
-        }
-
-        function orderContextChildren(items) {
-            var modelItems = [];
-
-            items.forEach(function(item) {
-                modelItems.push(item.model);
-            });
-
-            activeContext.children = items;
-            activeContext.model['children'] = modelItems;
-        }
-
-        function addItem(model) {
-            var newItem = new MenuTree(model);
-            activeContext.addChild(newItem);
-        }
-
-        function dropItem(item) {
-            item.drop();
-        }
-
-        function getBreadcrumb() {
-            if (typeof activeContext.getPath === 'function') {
-                var path = activeContext.getPath();
-                return path;
-            }
-        }
-
-        function getTypes() {
-            return menuTypes;
-        }
-
-
     }
 })();
