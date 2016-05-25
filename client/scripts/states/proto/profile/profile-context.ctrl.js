945d7a8f38305d2fe9b174596074054d527210f8
adam-s <adamsohn_1@hotmail.com>
2016-05-25 12:38:02 -0400

    profile

--- client/scripts/states/proto/profile/profile-context.ctrl.js
+++ client/scripts/states/proto/profile/profile-context.ctrl.js
@@ -6,59 +6,7 @@
 
     function profileContextCtrl() {
         var vm = this;
-        vm.path = [
-            {
-                title: 'Luigi\'s',
-                typeLabel: 'Menu root'
-            },
-            {
-                title: 'Food menu',
-                typeLabel: 'Section'
-            },
-            {
-                title: 'Coal oven pizza',
-                typeLabel: 'Section'
-            },
-            {
-                title: 'Tomato pie',
-                typeLabel: 'Menu item'
-            },
-            {
-                title: '16"',
-                typeLabel: 'Variation'
-            }
-        ];
-
-        vm.children = [
-            {
-                title: 'Included',
-                type: 'label'
-            },
-            {
-                title: 'Tomato sauce',
-                type: 'modifier'
-            },
-            {
-                title: 'Mozzarella cheese',
-                type: 'modifier'
-            },
-            {
-                title: 'Add toppings',
-                type: 'label'
-            },
-            {
-                title: 'Anchovies',
-                type: 'modifier'
-            },
-            {
-                title: 'Artichoke slices',
-                type: 'modifier'
-            },
-            {
-                title: 'Extra hot finger peppers',
-                type: 'modifier'
-            }
-        ]
-
+        vm.state = 'FL';
+        vm.chips = ['Pizza', 'Italian', 'Takeout'];
     }
 })();
